9d05c4f9d1dd81f6aac340114122b95c0db33b0a
Rene Crapaud <rene_ab_ing@outlook.com>
2023-02-20 21:59:15 -0600

    Agregar buscador por platillo

---
+++ src/components/Category.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="category-container" v-for="cat in categorias" :key="cat.idCategory">
+        <img :src="cat.strCategoryThumb" alt="Imagen de categoria">
+        <h6>{{ cat.strCategory }}</h6>
+        <p>{{ cat.strCategoryDescription }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"itemCategoria",
+    props:['categorias']
+}
+</script>
+<style scoped>
+.category-container{
+  border: 1px solid yellow;
+  padding: 50px;
+}
+</style>
---
+++ src/components/Meals.vue
@@ -0,0 +1,13 @@
+<template>
+    <div v-for="meal in mealsResp" :key="meal.id">
+        <img :src="meal.strMealThumb" alt="Imagen de categoria">
+        <p><b>{{ meal.strMeal }}</b></p>
+        <h6>Categoría: {{ meal.strCategory }} | Origen: {{ meal.strArea }}</h6>
+    </div>
+</template>
+<script>
+    export default{
+        name:"mealComp",
+        props:["mealsResp"]    
+    }
+</script>
